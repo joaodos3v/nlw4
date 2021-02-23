db3e1b9f5c3b62825ecce915da91c75d8724e629
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2021-02-22 23:33:13 -0300

    (feat): adicionar fontes do projeto e estilização global

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Button } from './components/Button';
+import './styles/global.css';
 
 function App() {
   return (
     <>
-      <Button color="blue">
-        Botão 1
-      </Button>
-      <Button color="red">
-        Botão 2
-      </Button>
-      <Button color="green">
-        Botão 3
-      </Button>
     </>
   );
 }
