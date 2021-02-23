13fb59c8b5cb9bbbe222f93176a31e078e1eb3cb
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2021-02-22 23:21:59 -0300

    (feat): adicionando exemplo de estado com contador

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 interface ButtonProps {
   color: string;
   children: string;
 }
 
 export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(1);
+
+  function increment() {
+    setCounter(prevCounter => prevCounter + 1);
+  }
+
   return (
-    <button type="button" style={{ backgroundColor: props.color }}>
-      {props.children}
+    <button
+      type="button"
+      style={{ backgroundColor: props.color, color: 'white' }}
+      onClick={increment}
+    >
+      {props.children} - Counter: {counter}
     </button>
   )
 }
