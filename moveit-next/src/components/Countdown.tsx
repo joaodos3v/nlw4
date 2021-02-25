3b4d42501727d4bd1e91ec5be4b76de978d371c6
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2021-02-24 21:14:30 -0300

    (feat): adicionar botão para iniciar ciclo e lógica de contador

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react';
+
 import styles from '../styles/components/Countdown.module.css';
 
+const TWENTY_FIVE_MINUTES_IN_SECONDS = 25 * 60;
+
 export function Countdown() {
+  const [time, setTime] = useState(TWENTY_FIVE_MINUTES_IN_SECONDS);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(prevTime => prevTime - 1)
+      }, 1000);
+    }
+  }, [active, time])
+
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+        Iniciar um ciclo
+      </button>
     </div>
   );
 }
