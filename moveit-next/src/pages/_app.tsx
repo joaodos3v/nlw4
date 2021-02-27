b14cae55f485695d8c263b1fd4af0dc47763df9b
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2021-02-27 18:10:25 -0300

    (feat): armazenar as informações do usuário via cookies e utilizá-las ao carregar a página com a SSR do Next.js

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -5,13 +5,9 @@
 
 import '../styles/global.css';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
@@ -6,30 +7,62 @@ import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import styles from '../styles/pages/Home.module.css';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+/**
+ * Obrigatoriamente, esta função precisa ser assíncrona em virtude da tipagem do Next.js
+ *   Além disso, este método permite que chamadas/conexões a serviços externos sejam feitas na...
+ * ...camada do Next.js, **ANTES** de montar a página (este método está disponível apenas em páginas).
+ *   Dessa forma, indexadores (como o Google) verão a página montada perfeitamente no momento de...
+ * ...indexar o sistema, o que otimiza o SEO do app.
+ */
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { level, currentExperience, challengesCompleted } = context.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
+
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider> {/* Note: este contexto depende do contexto de desafios e, por isso, obrigatoriamente precisa ser utilizado depois */}
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider> {/* Note: este contexto depende do contexto de desafios e, por isso, obrigatoriamente precisa ser utilizado depois */}
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
 }
