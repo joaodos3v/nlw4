153a22c40ee4bf8587cca39c36989e41cfa8827e
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2021-02-27 17:10:40 -0300

    (feat): finalizar funcionalidade de 'leveling' e adicionar contexto para Countdown

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/joaodos3v.png" alt="João Vitor Veronese Vieira"/>
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>João Vitor Veronese Vieira</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
---
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+import { ChallengesContext } from "./ChallengesContext";
+
+interface CountdownContextData {
+  minutes: number;
+  seconds: number;
+  hasFinished: boolean;
+  isActive: boolean;
+  startCountdown: () => void;
+  resetCountdown: () => void;
+};
+
+interface CountdownProviderProps {
+  children: ReactNode;
+};
+
+const TWENTY_FIVE_MINUTES_IN_SECONDS = 0.05 * 60;
+
+let countdownTimeout: NodeJS.Timeout;
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(TWENTY_FIVE_MINUTES_IN_SECONDS);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(TWENTY_FIVE_MINUTES_IN_SECONDS);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(prevTime => prevTime - 1)
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, time])
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  )
+}
